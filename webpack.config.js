b023a7af0a57acb822ce9bfcb629a4d4d55142ef
Gianluca Venturini <gianluca@stellarite.io>
2020-08-24 18:40:47 -0700

    Test list photos

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PROJECTS = (process.env.PROJECTS || '').split(',');
 const IS_WATCH = !!process.env.WATCH;
@@ -37,41 +36,6 @@ const moduleConfig = (tscOptions = {}) => ({
  * See: https://decembersoft.com/posts/how-to-fix-your-server-side-typescript-call-stack-with-webpack-bannerplugin/
  */
 const sourceMapBannerPlugin = new webpack.BannerPlugin({ banner: 'require("source-map-support").install();', raw: true, entryOnly: false });
-
-const htmlPlugin = new HtmlWebpackPlugin({ template: './src/frontend/index.html' });
-
-const frontendBuild = (name, entryFile, outputFile) => ({
-    name: name,
-    entry: entryFile,
-    output: {
-        path: __dirname + '/dist/www',
-        filename: outputFile
-    },
-    module: moduleConfig({ target: 'ES6', module: 'ES6' }),
-    mode: 'development',
-    devtool: 'cheap-module-eval-source-map',
-    resolve: resolveConfig,
-    watch: IS_WATCH,
-    devServer: {
-        historyApiFallback: true, // replace 404 page with index.html for client-side routing
-        port: 8000,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8080',
-                secure: false
-            }
-        }
-    },
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows to avoid bundling all of your
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
-    plugins: [htmlPlugin]
-});
 
 const backendBuild = (name, entryFile, outputFile = 'server.js') => {
     return {
